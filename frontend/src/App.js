75cc98b5d210b8d94bb89f548e921f57f78b73bb
richa shah <lavaformama@gmail.com>
2023-05-02 09:57:27 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginForm from './LoginForm';
+
+function App() {
+  return (
+    <div className="App">
+      <p>hello</p>
+     <LoginForm/>
+    </div>
+  );
+}
+
+export default App;
